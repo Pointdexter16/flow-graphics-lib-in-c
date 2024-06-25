9ad5d2b935dab1fe8001fa121e8454a1cf79adaf
Shehzail <abbasshehzail@gmail.com>
2024-06-26 00:19:05 +0530

    rect and circle

--- example.c
+++ example.c
@@ -1,23 +1,64 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <errno.h>
+#include<stdbool.h>
 #include<string.h>
 #include "flow.c"
 
 #define Width 800
-#define Heigth 600
+#define Height 600
+#define BACKGROUND_COLOR 0xFF303030
+#define WHITE 0xFFFFFFFF
+#define FOREGROUND_COLOR 0xFF2020FF
 
-static uint32_t pixels[Heigth * Width];
+#define ROWS 6
+#define COLUMNS 8
 
-int main()
-{
-    const char *file_path = "output1.ppm";
-    flow_fill(pixels, Width, Heigth, 0xFF00FF00);
-    Errno err = flow_save_to_ppm(pixels, Width, Heigth, file_path);
+static uint32_t pixels[Height * Width];
+
+bool checker_example(){
+    flow_fill(pixels,Width,Height,BACKGROUND_COLOR);
+    int w=(Width/COLUMNS);
+    int h=(Height/ROWS);
+    uint32_t color;
+    for(int x=0;x<COLUMNS;x++){
+        for(int y=0;y<ROWS;y++){
+            if((x+y)%2==0){
+                color=BACKGROUND_COLOR;
+            }
+            else{
+                color=0xFF2020FF;
+            }
+            flow_fill_rect(pixels,Width,Height,w*x,h*y,w,h,color);
+        }
+    }
+    const char *file_path = "checker.ppm";
+    Errno err = flow_save_to_ppm(pixels, Width, Height, file_path);
     if (err)
     {
         printf("couldn't save the file %s: %s\n", file_path, strerror(err));
-        return 1;
+        return false;
+    }
+    return true;
+}
+
+bool circle_example(){
+    flow_fill(pixels,Width,Height,BACKGROUND_COLOR);
+    flow_fill_circle(pixels,Width,Height,Width/2,Height/2,100,FOREGROUND_COLOR);
+    const char *file_path = "circle.ppm";
+    Errno err = flow_save_to_ppm(pixels, Width, Height, file_path);
+    if (err)
+    {
+        printf("couldn't save the file %s: %s\n", file_path, strerror(err));
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    if(!circle_example()){
+        return -1;
     }
     return 0;
 }
--- flow.c
+++ flow.c
@@ -5,7 +5,7 @@
 
 #ifndef FLOW_C_
 #define FLOW_C_
-#endif //FLOW_C_
+#endif 
 
 
 #define return_defer(value) \
@@ -53,3 +53,40 @@ defer:
         fclose(f);
     return result;
 }
+
+void flow_fill_rect(uint32_t *pixels, size_t pixel_width, size_t pixel_height, int x0, int y0, size_t w, size_t h, uint32_t color){
+    int y,x;
+    for(int dy=0; dy < (int)h; dy++){
+        y = y0 + dy;
+        if(0 <= y && y < (int)pixel_height){
+            for(int dx=0; dx < (int)w; dx++){
+                x = x0 + dx;
+                if(0 <= x && x < (int)pixel_width){
+                    pixels[y*pixel_width + x]=color;
+                }   
+            }
+        }
+    }
+}
+
+void flow_fill_circle(uint32_t *pixels, size_t pixel_width, size_t pixel_height, int cx, int cy, int r, uint32_t color){
+    int x0=cx-r;
+    int y0=cy-r;
+    int x1=cx+r;
+    int y1=cy+r;
+    for(int y=y0;y<=y1;y++){
+        if(0 <= y && y < (int)pixel_height){
+            for(int x=x0;x<=x1;x++){
+                if(0 <= x && x < (int)pixel_width){
+                    int dx=x-cx;
+                    int dy=y-cy;
+                    if(dx*dx + dy*dy<=r*r){
+                        pixels[pixel_width*y+x]=color;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
